629f26c4890995a17e67cfad06808159533091c3
El-Behery <dev.elbehery@gmail.com>
2023-04-19 03:32:19 +0200

    create app, install firebase- firebase-tools, create config folder and file and implement the code in the file

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+//! For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzGk8IqHfqY2JA1Mfr5MMWJ-J0CXxdFNg",
+  authDomain: "fir-course-b7d7e.firebaseapp.com",
+  projectId: "fir-course-b7d7e",
+  storageBucket: "fir-course-b7d7e.appspot.com",
+  messagingSenderId: "967375626435",
+  appId: "1:967375626435:web:b1042bd7c7fa9112c38276",
+  measurementId: "G-FSW6WS1069",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
